c2cb5ee9f45589d1008f05a449156d7d16d371dc
Agrim Mittal <agrim@mudrex.com>
2021-01-06 18:38:46 +0530

    (zsh) Update options

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -10,13 +10,12 @@
 # zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 # compdef w=-redirect-,-default-,-default- # Gives w <tab> autocomplete
 
-
 IFS=$' \n\t'
 
 # History
-HISTSIZE=10000 # Lines of history to keep in memory for current session
-HISTFILESIZE=10000 # Number of commands to save in the file
-SAVEHIST=10000 # Number of history entries to save to disk
+HISTSIZE=1000 # Lines of history to keep in memory for current session
+HISTFILESIZE=100000 # Number of commands to save in the file
+SAVEHIST=100000 # Number of history entries to save to disk
 HISTFILE=~/.zsh_history # Where to save history to disk
 HISTDUP=erase # Erase duplicates in the history file
 setopt hist_ignore_dups # Ignore duplicates
